fcdab3354182ee50fc39234369862dc8f02bfe9d
Bildiabo <105174451+Bildiabo@users.noreply.github.com>
2022-05-29 20:03:44 +0200

    added new feature where u can calculate the remaining balnce with some simplification to the code

--- src/calculator/main.java
+++ src/calculator/main.java
@@ -4,41 +4,56 @@ import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class main {
+    final static byte MONTHS_IN_YEAR = 12;
+    final static byte PERCENT = 100;
     public static void main(String[] args) {
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
 
         Scanner scanner = new Scanner(System.in);
         int principal;
-        float monthlyInterest;
-        int numberOfPayments;
-        while (true) {
-            System.out.print("Principal: ");
-            principal = scanner.nextInt();
-            if (principal >= 1_000 && principal <= 1_000_000)
-                break;
-            System.out.println("Enter a value between 1k to 1m $");
-        }
-        while (true) {
-            System.out.print("Annual Interest Rate:");
-            float annualInterest = scanner.nextFloat();
-            monthlyInterest = (annualInterest / PERCENT) / MONTHS_IN_YEAR;
-            if (annualInterest > 0 && annualInterest <= 30)
-                break;
-            System.out.println("enter a valid number bitch bigger than 0 to 30");
-        }
-        while (true) {
-            System.out.print("period (Years): ");
-            byte years = scanner.nextByte();
-            numberOfPayments = years * MONTHS_IN_YEAR;
-            if (years >=1 && years<=30)
-                break;
-            System.out.println("enter a valid number between 1 to 30");
-        }
-        double mortagage = principal * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)) /
-                (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+        float annualInterest;
+        byte years;
+        principal = (int) readNumber("principal; ",1_000,1_000_000);
+        annualInterest=(float)readNumber("Annual Interest Rate: ",1,30);
+        years=(byte)readNumber("period (Years): ",1,30);
+        double mortgage = calculateMortgage(principal, annualInterest, years);
+    }
+    public static double readNumber(String prompt, double min, double max){
+        Scanner scanner = new Scanner(System.in);
+        double value;
+    while (true) {
+        System.out.print(prompt);
+        value = scanner.nextInt();
+        if (value >= min && value <= max)
+            break;
+        System.out.println("Enter a value between" + min + " and " +max);
+    }
+    return value;
+    }
+    public static double calculateBalance (int principal,
+                                           float annualInterest,
+                                           byte years, short numberOfPaymentsMade){
+        double monthlyInterest = (annualInterest / PERCENT) / MONTHS_IN_YEAR;
+        short numberOfPayments =(short)( years * MONTHS_IN_YEAR);
+        double balance = principal *( Math.pow(1+monthlyInterest, numberOfPayments)-Math.pow(1+monthlyInterest,numberOfPaymentsMade))
+                /(Math.pow(1+monthlyInterest,numberOfPayments)-1);
+        String balanceFormatted = NumberFormat.getCurrencyInstance().format(balance);
+        System.out.print("Remaining Balance= "+balanceFormatted);
+        return balance;
+
+    }
+    public static double calculateMortgage(int principal,
+                                           float annualInterest,
+                                           byte years){
+        double monthlyInterest = (annualInterest / PERCENT) / MONTHS_IN_YEAR;
+        short numberOfPayments =(short)( years * MONTHS_IN_YEAR);
+        double mortagage = principal
+            * (monthlyInterest * Math.pow(1 + monthlyInterest, numberOfPayments)) /
+            (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
         String mortageFormatted = NumberFormat.getCurrencyInstance().format(mortagage);
         System.out.println("Mortagage:" + mortageFormatted);
+        return mortagage;
+
+
     }
 
 
